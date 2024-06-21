693caaa0b5a48d59d1fa31fd2a54ab8afc82e1f3
LukynkaCZE <jiraklukas058@seznam.cz>
2024-06-21 22:38:07 +0200

    damage type registry

--- src/main/kotlin/io/github/dockyardmc/registry/DamageType.kt
+++ src/main/kotlin/io/github/dockyardmc/registry/DamageType.kt
@@ -1,16 +1,77 @@
 package io.github.dockyardmc.registry
 
+import cz.lukynka.prettylog.log
 import io.github.dockyardmc.scroll.extensions.put
+import io.github.dockyardmc.utils.Resources
 import org.jglrxavpok.hephaistos.nbt.NBT
 import org.jglrxavpok.hephaistos.nbt.NBTCompound
+import org.jglrxavpok.hephaistos.parser.SNBTParser
+import java.io.StringReader
 
 object DamageTypes {
 
-    // Dockyard doesn't use vanilla damage types
-    var map = mutableMapOf<String, DamageType>(
-        "minecraft:generic" to DamageType(0.0f, "generic", "when_caused_by_living_non_player"),
-        "minecraft:generic_kill" to DamageType(0.0f, "genericKill", "when_caused_by_living_non_player"),
-    )
+    val map by lazy {
+        val vanillaEntry = Resources.getFile("vanillaregistry/damage_type.snbt").split("\n")
+        val list = mutableListOf<DamageType>()
+        vanillaEntry.forEach { pattern ->
+            val split = pattern.split(";")
+            val identifier = split[0]
+            val sNBT = split[1]
+
+            val nbt = (SNBTParser(StringReader(sNBT))).parse() as NBTCompound
+            list.add(DamageType.read(identifier, nbt))
+            log(identifier)
+        }
+        list.associateBy { it.identifier }
+    }
+
+    val ARROW = map["minecraft:arrow"]!!
+    val BAD_RESPAWN_POINT = map["minecraft:bad_respawn_point"]!!
+    val CACTUS = map["minecraft:cactus"]!!
+    val CAMPFIRE = map["minecraft:campfire"]!!
+    val CRAMMING = map["minecraft:cramming"]!!
+    val DRAGON_BREATH = map["minecraft:dragon_breath"]!!
+    val DROWN = map["minecraft:drown"]!!
+    val DRY_OUT = map["minecraft:dry_out"]!!
+    val EXPLOSION = map["minecraft:explosion"]!!
+    val FALL = map["minecraft:fall"]!!
+    val FALLING_ANVIL = map["minecraft:falling_anvil"]!!
+    val FALLING_BLOCK = map["minecraft:falling_block"]!!
+    val FALLING_STALACTITE = map["minecraft:falling_stalactite"]!!
+    val FIREBALL = map["minecraft:fireball"]!!
+    val FIREWORKS = map["minecraft:fireworks"]!!
+    val FLY_INTO_WALL = map["minecraft:fly_into_wall"]!!
+    val FREEZE = map["minecraft:freeze"]!!
+    val GENERIC = map["minecraft:generic"]!!
+    val GENERIC_KILL = map["minecraft:generic_kill"]!!
+    val HOT_FLOOR = map["minecraft:hot_floor"]!!
+    val IN_FIRE = map["minecraft:in_fire"]!!
+    val IN_WALL = map["minecraft:in_wall"]!!
+    val INDIRECT_MAGIC = map["minecraft:indirect_magic"]!!
+    val LAVA = map["minecraft:lava"]!!
+    val LIGHTNING_BOLT = map["minecraft:lightning_bolt"]!!
+    val MAGIC = map["minecraft:magic"]!!
+    val MOB_ATTACK = map["minecraft:mob_attack"]!!
+    val MOB_ATTACK_NO_AGGRO = map["minecraft:mob_attack_no_aggro"]!!
+    val MOB_PROJECTILE = map["minecraft:mob_projectile"]!!
+    val ON_FIRE = map["minecraft:on_fire"]!!
+    val OUT_OF_WORLD = map["minecraft:out_of_world"]!!
+    val OUTSIDE_BORDER = map["minecraft:outside_border"]!!
+    val PLAYER_ATTACK = map["minecraft:player_attack"]!!
+    val PLAYER_EXPLOSION = map["minecraft:player_explosion"]!!
+    val SONIC_BOOM = map["minecraft:sonic_boom"]!!
+    val SPIT = map["minecraft:spit"]!!
+    val STALAGMITE = map["minecraft:stalagmite"]!!
+    val STARVE = map["minecraft:starve"]!!
+    val STING = map["minecraft:sting"]!!
+    val SWEET_BERRY_BUSH = map["minecraft:sweet_berry_bush"]!!
+    val THORNS = map["minecraft:thorns"]!!
+    val THROWN = map["minecraft:thrown"]!!
+    val TRIDENT = map["minecraft:trident"]!!
+    val UNATTRIBUTED_FIREBALL = map["minecraft:unattributed_fireball"]!!
+    val WIND_CHARGE = map["minecraft:wind_charge"]!!
+    val WITHER = map["minecraft:wither"]!!
+    val WITHER_SKULL = map["minecraft:wither_skull"]!!
 
     lateinit var registryCache: Registry
 
@@ -20,24 +81,37 @@ object DamageTypes {
         registryCache = Registry("minecraft:damage_type", registryEntries)
     }
 
-    val GENERIC = map["minecraft:generic"]!!
-    val GENERIC_KILL = map["minecraft:generic_kill"]!!
-
     init {
         cacheRegistry()
     }
 }
 
 data class DamageType(
+    val identifier: String,
     val exhaustion: Float,
     val messageId: String,
-    val scaling: String
+    val scaling: String,
+    val effects: String?,
+    val deathMessageType: String?
 ) {
     fun toNBT(): NBTCompound {
         return NBT.Compound {
             it.put("exhaustion", this.exhaustion)
             it.put("message_id", this.messageId)
             it.put("scaling", this.scaling)
+        }
+    }
+
+    companion object {
+        fun read(identifier: String, nbt: NBTCompound): DamageType {
+            return DamageType(
+                identifier = identifier,
+                exhaustion = nbt.getFloat("exhaustion")!!,
+                messageId = nbt.getString("message_id")!!,
+                scaling = nbt.getString("scaling")!!,
+                effects = nbt.getString("effects"),
+                deathMessageType = nbt.getString("death_message_Type")
+            )
         }
     }
 }
