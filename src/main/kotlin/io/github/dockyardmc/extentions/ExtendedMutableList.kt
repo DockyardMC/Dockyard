ecfc47844be48486fee1106e0ec6b6b87cf43903
LukynkaCZE <jiraklukas058@seznam.cz>
2024-06-29 20:40:14 +0200

    change MutableList<Player> to List<Player> instead

--- src/main/kotlin/io/github/dockyardmc/extentions/ExtendedMutableList.kt
+++ src/main/kotlin/io/github/dockyardmc/extentions/ExtendedMutableList.kt
@@ -8,67 +8,67 @@ import io.github.dockyardmc.protocol.packets.ClientboundPacket
 import io.github.dockyardmc.scroll.Component
 import io.github.dockyardmc.scroll.extensions.toComponent
 
-fun MutableList<Player>.sendMessage(message: String) {
+fun List<Player>.sendMessage(message: String) {
     this.forEach { it.sendMessage(message) }
 }
 
-fun MutableList<Player>.sendMessage(message: Component) {
+fun List<Player>.sendMessage(message: Component) {
     this.forEach { it.sendMessage(message) }
 }
 
-fun MutableList<Player>.sendPacket(packet: ClientboundPacket) {
+fun List<Player>.sendPacket(packet: ClientboundPacket) {
     this.forEach { it.sendPacket(packet) }
 }
 
-fun MutableList<Player>.sendActionBar(message: String) {
+fun List<Player>.sendActionBar(message: String) {
     this.forEach { it.sendActionBar(message) }
 }
 
-fun MutableList<Player>.sendActionBar(message: Component) {
+fun List<Player>.sendActionBar(message: Component) {
     this.forEach { it.sendActionBar(message) }
 }
 
-fun MutableList<Player>.sendTitle(title: String, subtitle: String = "", fadeIn: Int = 10, stay: Int = 60, fadeOut: Int = 10) {
+fun List<Player>.sendTitle(title: String, subtitle: String = "", fadeIn: Int = 10, stay: Int = 60, fadeOut: Int = 10) {
     this.forEach { it.sendTitle(title, subtitle, fadeIn, stay, fadeOut) }
 }
 
-fun MutableList<Player>.clearTitle(reset: Boolean = true) {
+fun List<Player>.clearTitle(reset: Boolean = true) {
     this.forEach { it.clearTitle(reset) }
 }
 
-fun MutableList<Player>.setTabHeader(header: String) {
+fun List<Player>.setTabHeader(header: String) {
     this.forEach { it.tabListHeader.value = header.toComponent() }
 }
 
-fun MutableList<Player>.setTabFooter(footer: String) {
+fun List<Player>.setTabFooter(footer: String) {
     this.forEach { it.tabListFooter.value = footer.toComponent() }
 }
 
-fun MutableList<Player>.setGameMode(gameMode: GameMode) {
+fun List<Player>.setGameMode(gameMode: GameMode) {
     this.forEach { it.gameMode.value = gameMode }
 }
 
-fun MutableList<Entity>.addViewer(player: Player) {
+fun List<Entity>.addViewer(player: Player) {
     this.forEach { it.addViewer(player) }
 }
 
-fun MutableList<Entity>.removeViewer(player: Player, isDisconnect: Boolean = false) {
+fun List<Entity>.removeViewer(player: Player, isDisconnect: Boolean = false) {
     this.forEach { it.removeViewer(player, isDisconnect) }
 }
 
-fun MutableList<Player>.setCanFly(canFly: Boolean) {
+fun List<Player>.setCanFly(canFly: Boolean) {
     this.forEach { it.canFly.value = canFly }
 }
 
-fun MutableList<Player>.setIsFlying(isFlying: Boolean) {
+fun List<Player>.setIsFlying(isFlying: Boolean) {
     this.forEach { it.isFlying.value = isFlying }
 }
 
-fun MutableList<Player>.give(itemStack: ItemStack) {
+fun List<Player>.give(itemStack: ItemStack) {
     this.forEach { it.give(itemStack) }
 }
 
-fun MutableList<Player>.clearInventory() {
+fun List<Player>.clearInventory() {
     this.forEach { it.clearInventory() }
 }
 
