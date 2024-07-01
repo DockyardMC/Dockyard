a2adcf20d73727837c7655f96b40af476fb96f29
LukynkaCZE <jiraklukas058@seznam.cz>
2024-07-02 00:42:29 +0200

    cleanup teams code

--- src/main/kotlin/io/github/dockyardmc/protocol/packets/play/clientbound/ClientboundTeamsPacket.kt
+++ src/main/kotlin/io/github/dockyardmc/protocol/packets/play/clientbound/ClientboundTeamsPacket.kt
@@ -3,93 +3,72 @@ package io.github.dockyardmc.protocol.packets.play.clientbound
 import io.github.dockyardmc.annotations.ClientboundPacketInfo
 import io.github.dockyardmc.annotations.WikiVGEntry
 import io.github.dockyardmc.entities.Entity
-import io.github.dockyardmc.extentions.writeNBT
 import io.github.dockyardmc.extentions.writeStringArray
 import io.github.dockyardmc.extentions.writeUtf
-import io.github.dockyardmc.extentions.writeVarInt
 import io.github.dockyardmc.protocol.packets.ClientboundPacket
 import io.github.dockyardmc.protocol.packets.ProtocolState
 import io.github.dockyardmc.team.Team
-import io.github.dockyardmc.scroll.extensions.toComponent
+import io.github.dockyardmc.team.writeTeamInfo
 import io.netty.buffer.ByteBuf
-
-private fun writeTeamInfo(buffer: ByteBuf, team: Team) {
-    buffer.writeNBT(team.displayName.value.toNBT())
-    buffer.writeByte(team.flags.value)
-    buffer.writeUtf(team.nameTagVisibility.value.value)
-    buffer.writeUtf(team.collisionRule.value.value)
-    buffer.writeVarInt(team.color.value)
-    buffer.writeNBT((team.prefix.value ?: "".toComponent()).toNBT())
-    buffer.writeNBT((team.suffix.value ?: "".toComponent()).toNBT())
-}
-
-sealed class Action(val team: Team) {
-    abstract val id: Int
-
-    abstract fun write(buffer: ByteBuf)
-}
-
-class CreateTeam(team: Team): Action(team) {
-    override val id: Int = 0x00
-
-    override fun write(buffer: ByteBuf) {
-        writeTeamInfo(buffer, team)
-        buffer.writeStringArray(team.mapEntities())
-    }
-}
-
-class RemoveTeam(team: Team): Action(team) {
-    override val id = 0x01
-
-    override fun write(buffer: ByteBuf) {
-        // nothing to write
-    }
-}
-
-class UpdateTeam(team: Team): Action(team) {
-    override val id = 0x02
-
-    override fun write(buffer: ByteBuf) {
-        writeTeamInfo(buffer, team)
-    }
-}
-
-class AddEntities(team: Team, val entities: Collection<Entity>): Action(team) {
-    override val id = 0x03
-
-    init {
-        if (entities.any { it !in team.entities.values }) {
-            throw IllegalArgumentException("This entity is not in the team!")
-        }
-    }
-
-    override fun write(buffer: ByteBuf) {
-        buffer.writeStringArray(team.mapEntities())
-    }
-}
-
-class RemoveEntities(team: Team, val entities: Collection<Entity>): Action(team) {
-    override val id = 0x04
-
-    init {
-        if (entities.any { it in team.entities.values }) {
-            throw IllegalArgumentException("These entities are still in the team!")
-        }
-    }
-
-    override fun write(buffer: ByteBuf) {
-        buffer.writeStringArray(team.mapEntities())
-    }
-}
 
 @WikiVGEntry("Update Teams")
 @ClientboundPacketInfo(0x60, ProtocolState.PLAY)
-class ClientboundTeamsPacket(
-    action: Action
-): ClientboundPacket() {
+class ClientboundTeamsPacket(teamPacketAction: TeamPacketAction): ClientboundPacket() {
     init {
-        data.writeUtf(action.team.name)
-        data.writeByte(action.id)
-        action.write(data)
+        data.writeUtf(teamPacketAction.team.name)
+        data.writeByte(teamPacketAction.id)
+        teamPacketAction.write(data)
+    }
+}
+
+sealed interface TeamPacketAction {
+    val id: Int
+    val team: Team
+    fun write(buffer: ByteBuf)
+}
+
+class CreateTeamPacketAction(override val team: Team): TeamPacketAction {
+    override val id: Int = 0x00
+    override fun write(buffer: ByteBuf) {
+        buffer.writeTeamInfo(team)
+        buffer.writeStringArray(team.mapEntities())
+    }
+}
+
+class RemoveTeamPacketAction(override val team: Team): TeamPacketAction {
+    override val id = 0x01
+    override fun write(buffer: ByteBuf) {
+        // nothing to write in this packet
+    }
+}
+
+class UpdateTeamPacketAction(override val team: Team): TeamPacketAction {
+    override val id = 0x02
+    override fun write(buffer: ByteBuf) {
+        buffer.writeTeamInfo(team)
+    }
+}
+
+class AddEntitiesTeamPacketAction(override val team: Team, val entities: Collection<Entity>): TeamPacketAction {
+    override val id = 0x03
+
+    init {
+        require(!entities.any { it !in team.entities.values }) { "This entity is not in the team!" }
+    }
+
+    override fun write(buffer: ByteBuf) {
+        buffer.writeStringArray(team.mapEntities())
+    }
+}
+
+class RemoveEntitiesTeamPacketAction(override val team: Team, val entities: Collection<Entity>): TeamPacketAction {
+    override val id = 0x04
+
+    init {
+        require(!entities.any { it in team.entities.values }) { "These entities are still in the team!" }
+    }
+
+    override fun write(buffer: ByteBuf) {
+        buffer.writeStringArray(team.mapEntities())
     }
 }
--- src/main/kotlin/io/github/dockyardmc/team/Team.kt
+++ src/main/kotlin/io/github/dockyardmc/team/Team.kt
@@ -4,27 +4,27 @@ import io.github.dockyardmc.bindables.Bindable
 import io.github.dockyardmc.bindables.BindableMutableList
 import io.github.dockyardmc.entities.Entity
 import io.github.dockyardmc.extentions.sendPacket
+import io.github.dockyardmc.extentions.writeNBT
+import io.github.dockyardmc.extentions.writeUtf
+import io.github.dockyardmc.extentions.writeVarInt
 import io.github.dockyardmc.player.Player
 import io.github.dockyardmc.player.PlayerManager
-import io.github.dockyardmc.protocol.packets.play.clientbound.AddEntities
+import io.github.dockyardmc.protocol.packets.play.clientbound.AddEntitiesTeamPacketAction
 import io.github.dockyardmc.protocol.packets.play.clientbound.ClientboundTeamsPacket
-import io.github.dockyardmc.protocol.packets.play.clientbound.UpdateTeam
+import io.github.dockyardmc.protocol.packets.play.clientbound.UpdateTeamPacketAction
 import io.github.dockyardmc.scroll.Component
+import io.github.dockyardmc.scroll.TextColor
 import io.github.dockyardmc.scroll.extensions.toComponent
+import io.netty.buffer.ByteBuf
 
-object Flags {
-    const val ALLOW_FRIENDLY_FIRE = 0x01
-    const val SEE_INVISIBLE_TEAMMATES = 0x02
-}
-
-enum class NameTagVisibility(val value: String) {
+enum class TeamNameTagVisibility(val value: String) {
     VISIBLE("always"),
     HIDE_OTHER_TEAMS("hideForOtherTeams"),
     HIDE_OWN_TEAM("hideForOwnTeam"),
     HIDDEN("never")
 }
 
-enum class CollisionRule(val value: String) {
+enum class TeamCollisionRule(val value: String) {
     ALWAYS("always"),
     PUSH_OTHER_TEAMS("pushOtherTeams"),
     PUSH_OWN_TEAM("pushOwnTeam"),
@@ -35,29 +35,53 @@ class Team(
     val name: String,
     val displayName: Bindable<Component> = Bindable(name.toComponent()),
     val flags: Bindable<Int> = Bindable(0x00),
-    val nameTagVisibility: Bindable<NameTagVisibility> = Bindable(NameTagVisibility.VISIBLE),
-    val collisionRule: Bindable<CollisionRule> = Bindable(CollisionRule.ALWAYS),
+    val teamNameTagVisibility: Bindable<TeamNameTagVisibility> = Bindable(TeamNameTagVisibility.VISIBLE),
+    val teamCollisionRule: Bindable<TeamCollisionRule> = Bindable(TeamCollisionRule.ALWAYS),
     val color: Bindable<Int> = Bindable(15),
     val prefix: Bindable<Component?> = Bindable(null),
     val suffix: Bindable<Component?> = Bindable(null)
 ) {
+
+    constructor(
+        name: String,
+        displayName: String,
+        flags: Int,
+        teamNameTagVisibility: TeamNameTagVisibility,
+        teamCollisionRule: TeamCollisionRule,
+        color: TextColor,
+        prefix: String? = null,
+        suffix: String? = null
+    ): this(
+        name,
+        Bindable<Component>(displayName.toComponent()),
+        Bindable<Int>(flags),
+        Bindable<TeamNameTagVisibility>(teamNameTagVisibility),
+        Bindable<TeamCollisionRule>(teamCollisionRule),
+        Bindable<Int>(color.ordinal),
+        Bindable<Component?>(prefix?.toComponent()),
+        Bindable<Component?>(suffix?.toComponent())
+    )
+
     val entities = BindableMutableList<Entity>()
 
+    companion object {
+        const val ALLOW_FRIENDLY_FIRE = 0x01
+        const val SEE_INVISIBLE_TEAMMATES = 0x02
+    }
+
     init {
-        displayName.valueChanged { updateViewers() }
-        flags.valueChanged { updateViewers() }
-        nameTagVisibility.valueChanged { updateViewers() }
-        collisionRule.valueChanged { updateViewers() }
-        color.valueChanged { updateViewers() }
-        prefix.valueChanged { updateViewers() }
-        suffix.valueChanged { updateViewers() }
+        displayName.valueChanged { sendTeamUpdatePacket() }
+        flags.valueChanged { sendTeamUpdatePacket() }
+        teamNameTagVisibility.valueChanged { sendTeamUpdatePacket() }
+        teamCollisionRule.valueChanged { sendTeamUpdatePacket() }
+        color.valueChanged { sendTeamUpdatePacket() }
+        prefix.valueChanged { sendTeamUpdatePacket() }
+        suffix.valueChanged { sendTeamUpdatePacket() }
 
         entities.itemAdded { event ->
-            if (event.item.team != this) {
-                throw IllegalArgumentException("Entity is still in another team!")
-            }
+            require(event.item.team == this) { "Entity is still in another team!" }
 
-            val packet = ClientboundTeamsPacket(AddEntities(this, listOf(event.item)))
+            val packet = ClientboundTeamsPacket(AddEntitiesTeamPacketAction(this, listOf(event.item)))
             event.item.viewers.forEach { it.sendPacket(packet) }
         }
     }
@@ -72,8 +96,18 @@ class Team(
         }
     }
 
-    fun updateViewers() {
-        val packet = ClientboundTeamsPacket(UpdateTeam(this))
+    fun sendTeamUpdatePacket() {
+        val packet = ClientboundTeamsPacket(UpdateTeamPacketAction(this))
         PlayerManager.players.sendPacket(packet)
     }
+}
+
+fun ByteBuf.writeTeamInfo(team: Team) {
+    this.writeNBT(team.displayName.value.toNBT())
+    this.writeByte(team.flags.value)
+    this.writeUtf(team.teamNameTagVisibility.value.value)
+    this.writeUtf(team.teamCollisionRule.value.value)
+    this.writeVarInt(team.color.value)
+    this.writeNBT((team.prefix.value ?: "".toComponent()).toNBT())
+    this.writeNBT((team.suffix.value ?: "".toComponent()).toNBT())
 }
