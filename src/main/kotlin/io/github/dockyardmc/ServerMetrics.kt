55445a8bbd3266e2a648d69de9d03c2c7c77561c
Nazar <youtube@superneon4ik.me>
2024-08-13 23:36:17 +0300

    Fix mspt calculation failing cuz of localization

--- src/main/kotlin/io/github/dockyardmc/ServerMetrics.kt
+++ src/main/kotlin/io/github/dockyardmc/ServerMetrics.kt
@@ -2,7 +2,6 @@ package io.github.dockyardmc
 
 import io.github.dockyardmc.events.Events
 import io.github.dockyardmc.events.ServerTickEvent
-import io.github.dockyardmc.extentions.truncate
 import io.github.dockyardmc.periodic.Period
 import io.github.dockyardmc.periodic.SecondPeriod
 import java.time.Instant
@@ -43,10 +42,7 @@ object ServerMetrics {
             val diff = Instant.now().toEpochMilli() - timeSinceLastTick.toEpochMilli()
             averages.add(diff)
             timeSinceLastTick = Instant.now()
-            millisecondsPerTick = averages.average().truncate(1).toDouble()
-            if(millisecondsPerTick < 50) millisecondsPerTick = 50.0
-
-
+            millisecondsPerTick = Math.round(averages.average() * 10) / 10.0
         }
     }
 }
