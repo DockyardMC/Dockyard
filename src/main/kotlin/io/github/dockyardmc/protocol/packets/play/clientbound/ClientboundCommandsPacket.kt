66febc41c56225567b82ac2d7e1adc79c5b9137d
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2024-09-02 02:29:19 +0200

    basic command node structure

--- src/main/kotlin/io/github/dockyardmc/protocol/packets/play/clientbound/ClientboundCommandsPacket.kt
+++ src/main/kotlin/io/github/dockyardmc/protocol/packets/play/clientbound/ClientboundCommandsPacket.kt
@@ -2,15 +2,16 @@ package io.github.dockyardmc.protocol.packets.play.clientbound
 
 import io.github.dockyardmc.annotations.ClientboundPacketInfo
 import io.github.dockyardmc.annotations.WikiVGEntry
+import io.github.dockyardmc.commands.CommandNode
+import io.github.dockyardmc.commands.writeCommands
 import io.github.dockyardmc.protocol.packets.ClientboundPacket
 import io.github.dockyardmc.protocol.packets.ProtocolState
 
 @WikiVGEntry("Commands")
 @ClientboundPacketInfo(0x11, ProtocolState.PLAY)
-class ClientboundCommandsPacket: ClientboundPacket() {
+class ClientboundCommandsPacket(val commands: MutableMap<Int, CommandNode>): ClientboundPacket() {
 
     init {
-        //data is the buffer
-
+        data.writeCommands(commands)
     }
 }
