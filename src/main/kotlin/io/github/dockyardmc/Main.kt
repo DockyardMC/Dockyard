45430b7f77ee3e0feac0c37a55b78896a54b0a78
LukynkaCZE <jiraklukas058@seznam.cz>
2024-09-16 02:15:54 +0200

    add totem animation

--- src/main/kotlin/io/github/dockyardmc/Main.kt
+++ src/main/kotlin/io/github/dockyardmc/Main.kt
@@ -6,33 +6,32 @@ import io.github.dockyardmc.commands.StringArgument
 import io.github.dockyardmc.commands.SuggestionProvider
 import io.github.dockyardmc.datagen.EventsDocumentationGenerator
 import io.github.dockyardmc.datagen.VerifyPacketIds
-import io.github.dockyardmc.entities.BlockDisplay
-import io.github.dockyardmc.entities.EntityManager.spawnEntity
 import io.github.dockyardmc.events.Events
 import io.github.dockyardmc.events.PlayerJoinEvent
 import io.github.dockyardmc.events.PlayerLeaveEvent
 import io.github.dockyardmc.extentions.broadcastMessage
-import io.github.dockyardmc.player.*
-import io.github.dockyardmc.registry.*
+import io.github.dockyardmc.inventory.give
+import io.github.dockyardmc.item.ItemStack
+import io.github.dockyardmc.player.GameMode
+import io.github.dockyardmc.player.add
+import io.github.dockyardmc.registry.Items
+import io.github.dockyardmc.registry.PotionEffects
+import io.github.dockyardmc.registry.addPotionEffect
 import io.github.dockyardmc.resourcepack.addResourcepack
 import io.github.dockyardmc.resourcepack.removeResourcepack
-import io.github.dockyardmc.runnables.runLaterAsync
 import io.github.dockyardmc.utils.DebugScoreboard
-import io.github.dockyardmc.utils.Vector3f
-import io.github.dockyardmc.utils.debug
-import java.util.Vector
 
 // This is just testing/development environment.
 // To properly use dockyard, visit https://dockyardmc.github.io/Wiki/wiki/quick-start.html
 
 fun main(args: Array<String>) {
 
-    if(args.contains("validate-packets")) {
+    if (args.contains("validate-packets")) {
         VerifyPacketIds()
         return
     }
 
-    if(args.contains("event-documentation")) {
+    if (args.contains("event-documentation")) {
         EventsDocumentationGenerator()
         return
     }
@@ -60,16 +59,13 @@ fun main(args: Array<String>) {
 
     Commands.add("/test") {
         execute {
-            val player = it.getPlayerOrThrow()
-            val test = player.world.spawnEntity(BlockDisplay(player.location)) as BlockDisplay
-            test.autoViewable = false
-            test.block.value = Blocks.REDSTONE_BLOCK
-            test.translateTo(Vector3f(-0.5f), 0)
-            test.addViewer(player)
-            runLaterAsync(1) {
-                test.translateTo(Vector3f(0f, 2f, 0f), 20)
-                test.scaleTo(0f, 0f, 0f, 20)
-            }
+            it.getPlayerOrThrow().give(ItemStack(Items.TOTEM_OF_UNDYING).apply { customModelData.value = 1 })
+        }
+    }
+
+    Commands.add("/totem") {
+        execute {
+            it.getPlayerOrThrow().playTotemAnimation(1)
         }
     }
 
