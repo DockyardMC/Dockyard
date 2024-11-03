47b69cb515f7bad985afc727de64de9d26232e59
Aso <53526710+AsoDesu@users.noreply.github.com>
2024-11-03 14:34:37 +0000

    feat(events): additional EventFilter utilities

--- src/main/kotlin/io/github/dockyardmc/events/system/EventFilter.kt
+++ src/main/kotlin/io/github/dockyardmc/events/system/EventFilter.kt
@@ -1,6 +1,9 @@
 package io.github.dockyardmc.events.system
 
+import io.github.dockyardmc.entities.Entity
 import io.github.dockyardmc.events.Event
+import io.github.dockyardmc.player.Player
+import io.github.dockyardmc.world.World
 
 fun interface EventFilter {
     /**
@@ -15,5 +18,14 @@ fun interface EventFilter {
          * An empty EventFilter, always allows events through
          */
         fun empty() = EventFilter { true }
+        fun isGlobalOr(filter: EventFilter) = EventFilter { it.context.isGlobalEvent || filter.check(it) }
+
+        fun containsPlayer(obj: Player) = isGlobalOr { it.context.players.contains(obj) }
+        fun containsEntity(obj: Entity) = isGlobalOr { it.context.entities.contains(obj) }
+        fun containsWorld(obj: World) = isGlobalOr { it.context.worlds.contains(obj) }
+        fun containsObject(obj: Any) = isGlobalOr { it.context.other.contains(obj) }
+
+        fun all(vararg filters: EventFilter) = EventFilter { evt -> filters.all { it.check(evt) } }
+        fun any(vararg filters: EventFilter) = EventFilter { evt -> filters.any { it.check(evt) } }
     }
 }
