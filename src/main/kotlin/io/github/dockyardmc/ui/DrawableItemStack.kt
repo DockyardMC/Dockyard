9af0bb40122cbe865228e058a8ff40efb76ed531
LukynkaCZE <jiraklukas058@seznam.cz>
2024-12-29 18:12:15 +0100

    fix drawableItemStack

--- src/main/kotlin/io/github/dockyardmc/ui/DrawableItemStack.kt
+++ src/main/kotlin/io/github/dockyardmc/ui/DrawableItemStack.kt
@@ -1,6 +1,5 @@
 package io.github.dockyardmc.ui
 
-import io.github.dockyardmc.item.ItemComponent
 import io.github.dockyardmc.item.ItemStack
 import io.github.dockyardmc.player.Player
 import io.github.dockyardmc.registry.registries.Item
@@ -16,20 +15,6 @@ class DrawableItemStack {
 
     fun withItem(item: Item, amount: Int = 1) {
         this.itemStack = item.toItemStack(amount)
-    }
-
-    fun withName(name: String) {
-        itemStack.withDisplayName(name)
-    }
-
-    fun addLoreLine(lore: String) {
-        val mut = itemStack.existingMeta?.lore?.toMutableList() ?: mutableListOf()
-        mut.add(lore)
-        itemStack.withLore(mut)
-    }
-
-    fun withComponent(component: ItemComponent) {
-        itemStack.withComponent(component)
     }
 
     fun onClick(unit: (Player, DrawableClickType) -> Unit) {
