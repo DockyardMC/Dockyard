1b99e0560f3c1e001be24ef701802169294aafec
LukynkaCZE <jiraklukas058@seznam.cz>
2025-03-16 21:28:57 +0100

    le buffér

--- src/main/kotlin/io/github/dockyardmc/protocol/packets/play/clientbound/ClientboundStopSoundPacket.kt
+++ src/main/kotlin/io/github/dockyardmc/protocol/packets/play/clientbound/ClientboundStopSoundPacket.kt
@@ -8,12 +8,12 @@ import io.github.dockyardmc.protocol.packets.ClientboundPacket
 class ClientboundStopSoundPacket(flags: Byte, category: SoundCategory?, sound: String?): ClientboundPacket() {
 
     init {
-        data.writeByte(flags)
+        buffer.writeByte(flags)
         if((flags == 3.toByte() || flags == 1.toByte()) && category != null) {
-            data.writeVarInt(category.ordinal)
+            buffer.writeVarInt(category.ordinal)
         }
         if((flags == 2.toByte() || flags == 3.toByte()) && sound != null) {
-            data.writeString(sound)
+            buffer.writeString(sound)
         }
     }
 
