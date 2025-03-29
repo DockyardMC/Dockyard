c63b5ccd7b95f26c7ccf53e064641778de28b1e9
LukynkaCZE <jiraklukas058@seznam.cz>
2025-03-29 23:43:50 +0100

    more components

---
+++ src/main/kotlin/io/github/dockyardmc/data/components/UseRemainderComponent.kt
@@ -0,0 +1,19 @@
+package io.github.dockyardmc.data.components
+
+import io.github.dockyardmc.data.DataComponent
+import io.github.dockyardmc.item.ItemStack
+import io.github.dockyardmc.protocol.NetworkReadable
+import io.netty.buffer.ByteBuf
+
+class UseRemainderComponent(val remained: ItemStack): DataComponent() {
+
+    override fun write(buffer: ByteBuf) {
+        remained.write(buffer)
+    }
+
+    companion object: NetworkReadable<UseRemainderComponent> {
+        override fun read(buffer: ByteBuf): UseRemainderComponent {
+            return UseRemainderComponent(ItemStack.read(buffer))
+        }
+    }
+}
