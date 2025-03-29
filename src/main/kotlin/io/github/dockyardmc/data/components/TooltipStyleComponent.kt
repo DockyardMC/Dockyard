07159105888cdc1fc0efd58d7d0952e3cb6ab783
LukynkaCZE <jiraklukas058@seznam.cz>
2025-03-29 23:44:16 +0100

    forgot to finish this

--- src/main/kotlin/io/github/dockyardmc/data/components/TooltipStyleComponent.kt
+++ src/main/kotlin/io/github/dockyardmc/data/components/TooltipStyleComponent.kt
@@ -1,6 +1,7 @@
 package io.github.dockyardmc.data.components
 
 import io.github.dockyardmc.data.DataComponent
+import io.github.dockyardmc.extentions.readString
 import io.github.dockyardmc.extentions.writeString
 import io.github.dockyardmc.protocol.NetworkReadable
 import io.netty.buffer.ByteBuf
@@ -13,7 +14,7 @@ class TooltipStyleComponent(val style: String): DataComponent() {
 
     companion object: NetworkReadable<TooltipStyleComponent> {
         override fun read(buffer: ByteBuf): TooltipStyleComponent {
-            return TooltipStyleComponent()
+            return TooltipStyleComponent(buffer.readString())
         }
     }
 
