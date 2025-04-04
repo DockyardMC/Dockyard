6d8a1f27c74bd353ec11f920b631744177a61dc4
p1k0chu <anatoliyid0207@gmail.com>
2025-04-04 22:12:06 +0300

    feat: add AdvancementBuilder for advancements

---
+++ src/main/kotlin/io/github/dockyardmc/advancement/AdvancementBuilder.kt
@@ -0,0 +1,105 @@
+package io.github.dockyardmc.advancement
+
+import io.github.dockyardmc.item.ItemStack
+import io.github.dockyardmc.maths.vectors.Vector2f
+import io.github.dockyardmc.registry.Items
+import io.github.dockyardmc.registry.registries.Item
+import io.github.dockyardmc.scroll.extensions.toComponent
+import io.github.dockyardmc.utils.debug
+
+/**
+ * Build the [Advancement] WITH [AdvancementDisplay]
+ */
+class AdvancementBuilder(val id: String) {
+    var parentId: String? = null
+    val requirements = mutableListOf<List<String>>()
+
+    var title: String = ""
+    var description: String = ""
+    var icon: ItemStack = Items.PAPER.toItemStack()
+    var frame: AdvancementFrame = AdvancementFrame.TASK
+    var showToast: Boolean = true
+    var isHidden: Boolean = false
+    var background: String? = null
+
+    var x: Float = 0f
+    var y: Float = 0f
+
+    fun withParent(id: String?) {
+        this.parentId = id
+    }
+
+    fun withTitle(title: String) {
+        this.title = title
+    }
+
+    fun withDescription(description: String) {
+        this.description = description
+    }
+
+    fun withIcon(icon: ItemStack) {
+        this.icon = icon
+    }
+
+    fun withIcon(icon: Item) {
+        this.icon = icon.toItemStack()
+    }
+
+    fun withFrame(frame: AdvancementFrame) {
+        this.frame = frame
+    }
+
+    fun useToast(showToast: Boolean) {
+        this.showToast = showToast
+    }
+
+    fun withHidden(isHidden: Boolean) {
+        this.isHidden = isHidden
+    }
+
+    fun withBackground(background: String?) {
+        this.background = background
+    }
+
+    fun withBackground(background: Item) {
+        val id = background.identifier
+        debug("id = $id // BACKGROUND")
+        TODO()
+    }
+
+    fun withPosition(x: Float, y: Float) {
+        this.x = x
+        this.y = y
+    }
+    fun withPosition(vec: Vector2f) {
+        this.x = vec.x
+        this.y = vec.y
+    }
+
+    fun withRequirement(req: String) {
+        this.requirements += listOf(req)
+    }
+
+    fun withRequirementsAnyOf(requirements: List<String>) {
+        this.requirements += requirements
+    }
+
+    fun build(): Advancement {
+        return Advancement(
+            id,
+            parentId,
+            AdvancementDisplay(
+                title.toComponent(), description.toComponent(), icon,
+                frame, showToast, isHidden,
+                background, x, y
+            ),
+            requirements
+        )
+    }
+}
+
+fun advancement(id: String, builder: AdvancementBuilder.() -> Unit): Advancement {
+    val adv = AdvancementBuilder(id)
+    builder.invoke(adv)
+    return adv.build()
+}
