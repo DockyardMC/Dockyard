0ee333facccaa9e52838b8d9e9793f1be40ebad4
LukynkaCZE <jiraklukas058@seznam.cz>
2025-04-18 20:12:08 +0200

    add way to get nearest highest, lowest points

---
+++ src/test/kotlin/io/github/dockyard/tests/location/HighestLowestPointTest.kt
@@ -0,0 +1,75 @@
+package io.github.dockyard.tests.location
+
+import io.github.dockyard.tests.TestServer
+import io.github.dockyardmc.location.Location
+import io.github.dockyardmc.registry.Blocks
+import io.github.dockyardmc.world.WorldManager
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class HighestLowestPointTest {
+
+    @BeforeTest
+    fun prepare() {
+        TestServer.getOrSetupServer()
+    }
+
+    @AfterTest
+    fun cleanup() {
+    }
+
+    @Test
+    fun testLowestPoint() {
+        val world = WorldManager.mainWorld
+
+        world.setBlock(0, 0, 0, Blocks.STONE)
+        val zeroZeroZero = Location(0, 0, 0, world)
+
+        assertEquals(zeroZeroZero, zeroZeroZero.closestNonAirBelow?.second)
+
+        world.setBlock(0, 0, 0, Blocks.AIR)
+        world.setBlock(0, -1, 0, Blocks.AIR)
+        world.setBlock(0, -30, 0, Blocks.STONE)
+
+        assertEquals(world.locationAt(0, -30, 0), zeroZeroZero.closestNonAirBelow?.second)
+
+        world.setBlock(0, -30, 0, Blocks.AIR)
+
+        assertEquals(null, zeroZeroZero.closestNonAirBelow)
+
+        world.setBlock(0, -30, 0, Blocks.SHORT_GRASS)
+
+        assertEquals(world.locationAt(0, -30, 0), zeroZeroZero.closestNonAirBelow?.second)
+        assertEquals(null, zeroZeroZero.closestSolidBelow)
+
+        world.setBlock(0, -30, 0, Blocks.AIR)
+    }
+
+    @Test
+    fun testHighestPoint() {
+        val world = WorldManager.mainWorld
+
+        world.setBlock(0, 0, 0, Blocks.STONE)
+        val zeroZeroZero = Location(0, 0, 0, world)
+
+        assertEquals(zeroZeroZero, zeroZeroZero.closestNonAirAbove?.second)
+
+        world.setBlock(0, 0, 0, Blocks.AIR)
+        world.setBlock(0, 30, 0, Blocks.STONE)
+
+        assertEquals(world.locationAt(0, 30, 0), zeroZeroZero.closestNonAirAbove?.second)
+
+        world.setBlock(0, 30, 0, Blocks.AIR)
+
+        assertEquals(null, zeroZeroZero.closestNonAirAbove)
+
+        world.setBlock(0, 30, 0, Blocks.SHORT_GRASS)
+
+        assertEquals(world.locationAt(0, 30, 0), zeroZeroZero.closestNonAirAbove?.second)
+        assertEquals(null, zeroZeroZero.closestSolidAbove)
+
+        world.setBlock(0, 30, 0, Blocks.AIR)
+    }
+}
