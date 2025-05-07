dbc86e734a2ecd4bc536e669c9c9281bc4cbdf80
LukynkaCZE <jiraklukas058@seznam.cz>
2025-05-07 19:54:52 +0200

    add support for `LegacyTextComponent`

--- src/main/kotlin/io/github/dockyardmc/utils/RangeColorMapper.kt
+++ src/main/kotlin/io/github/dockyardmc/utils/RangeColorMapper.kt
@@ -1,13 +1,17 @@
 package io.github.dockyardmc.utils
 
 import io.github.dockyardmc.scroll.CustomColor
+import io.github.dockyardmc.scroll.LegacyTextColor
 
 data class RangeColorMapper(val steps: Collection<Step>, val defaultColor: CustomColor = CustomColor.fromHex("#FFFFFF")) {
+
+    constructor(defaultColor: CustomColor = CustomColor.fromHex("#FFFFFF"), vararg steps: Step) : this(steps.toList(), defaultColor)
 
     private val sortedSteps = steps.sortedByDescending { step -> step.aboveEquals }
 
     data class Step(val aboveEquals: Double, val color: CustomColor) {
         constructor(aboveEquals: Double, color: String) : this(aboveEquals, CustomColor.fromHex(color))
+        constructor(aboveEquals: Double, color: LegacyTextColor) : this(aboveEquals, CustomColor.fromHex(color.hex))
     }
 
     operator fun get(string: String, value: Double): String {
