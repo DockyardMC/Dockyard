24136c806a687adb858cee29c2e79b6de0a7212f
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2025-05-07 15:02:22 +0200

    Removed unused `toUnsignedLong`
    Co-authored-by: p1k0chu <64609869+p1k0chu@users.noreply.github.com>

--- src/main/kotlin/io/github/dockyardmc/extentions/ExtendedEnum.kt
+++ src/main/kotlin/io/github/dockyardmc/extentions/ExtendedEnum.kt
@@ -1,7 +1,3 @@
 package io.github.dockyardmc.extentions
 
 inline fun <reified T : Enum<T>> enumRandom(): T = enumValues<T>().random()
-
-fun toUnsignedLong(x: Int): Long {
-    return (x.toLong()) and 0xffffffffL
-}
