1526e94212d6cb4a0996339b3eb60b1fee465a02
p1k0chu <anatoliyid0207@gmail.com>
2025-05-28 14:26:13 +0300

    update dialogs

--- src/main/kotlin/io/github/dockyardmc/protocol/packets/play/serverbound/ServerboundCustomClickActionPacket.kt
+++ src/main/kotlin/io/github/dockyardmc/protocol/packets/play/serverbound/ServerboundCustomClickActionPacket.kt
@@ -2,6 +2,7 @@ package io.github.dockyardmc.protocol.packets.play.serverbound
 
 import io.github.dockyardmc.events.CustomClickActionEvent
 import io.github.dockyardmc.events.Events
+import io.github.dockyardmc.extentions.readNBT
 import io.github.dockyardmc.extentions.readString
 import io.github.dockyardmc.protocol.NetworkReadable
 import io.github.dockyardmc.protocol.PlayerNetworkManager
@@ -9,13 +10,14 @@ import io.github.dockyardmc.protocol.packets.ServerboundPacket
 import io.github.dockyardmc.protocol.readOptional
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
+import org.jglrxavpok.hephaistos.nbt.NBT
 
-class ServerboundCustomClickActionPacket(val id: String, val payload: String?) : ServerboundPacket {
+class ServerboundCustomClickActionPacket(val id: String, val payload: NBT?) : ServerboundPacket {
     companion object : NetworkReadable<ServerboundCustomClickActionPacket> {
         override fun read(buffer: ByteBuf): ServerboundCustomClickActionPacket {
             return ServerboundCustomClickActionPacket(
                 buffer.readString(),
-                buffer.readOptional { it.readString() }
+                buffer.readOptional { it.readNBT() }
             )
         }
 
