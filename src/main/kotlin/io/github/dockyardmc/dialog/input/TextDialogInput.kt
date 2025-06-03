4679c65489b3e93c161f2df9c41d894533dd8079
p1k0chu <anatoliyid0207@gmail.com>
2025-06-03 15:08:04 +0300

    more 1.21.6 pre-1 stuff for dialogs

--- src/main/kotlin/io/github/dockyardmc/dialog/input/TextDialogInput.kt
+++ src/main/kotlin/io/github/dockyardmc/dialog/input/TextDialogInput.kt
@@ -1,8 +1,10 @@
 package io.github.dockyardmc.dialog.input
 
+import io.github.dockyardmc.protocol.NbtWritable
 import io.github.dockyardmc.registry.DialogInputTypes
 import io.github.dockyardmc.registry.registries.DialogInputType
 import io.github.dockyardmc.scroll.extensions.put
+import org.jglrxavpok.hephaistos.nbt.NBT
 import org.jglrxavpok.hephaistos.nbt.NBTCompound
 
 class TextDialogInput(
@@ -11,14 +13,43 @@ class TextDialogInput(
     val width: Int = 200,
     val labelVisible: Boolean = true,
     val initial: String = "",
+    val maxLength: Int = 32,
+    val multiline: Multiline?,
 ) : DialogInput() {
     override val type: DialogInputType = DialogInputTypes.TEXT
+
+    init {
+        if (maxLength < 0) throw IllegalArgumentException("maxLength must be positive")
+    }
 
     override fun getNbt(): NBTCompound {
         return super.getNbt().kmodify {
             put("width", width)
             put("label_visible", labelVisible)
             put("initial", initial)
+            put("max_length", maxLength)
+            multiline?.let { put("multiline", it.getNbt()) }
+        }
+    }
+
+    class Multiline(
+        val maxLines: Int?,
+        val height: Int?,
+    ) : NbtWritable {
+        init {
+            maxLines?.let {
+                if (it < 0) throw IllegalArgumentException("maxLines must be positive")
+            }
+            height?.let {
+                if (it < 0) throw IllegalArgumentException("height must be positive")
+            }
+        }
+
+        override fun getNbt(): NBTCompound {
+            return NBT.Compound {
+                it.put("max_lines", maxLines)
+                it.put("height", height)
+            }
         }
     }
 }
