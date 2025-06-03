386e4e2a31a93e6faf31611e22fe91c97539ffc8
p1k0chu <anatoliyid0207@gmail.com>
2025-06-03 22:09:30 +0300

    did i really forget to call super in dialog bodies getNbt()???????

--- src/main/kotlin/io/github/dockyardmc/dialog/body/DialogBody.kt
+++ src/main/kotlin/io/github/dockyardmc/dialog/body/DialogBody.kt
@@ -4,11 +4,12 @@ import io.github.dockyardmc.protocol.NbtWritable
 import io.github.dockyardmc.registry.registries.DialogBodyType
 import io.github.dockyardmc.scroll.extensions.put
 import org.jglrxavpok.hephaistos.nbt.NBT
+import org.jglrxavpok.hephaistos.nbt.NBTCompound
 
 sealed class DialogBody : NbtWritable {
     abstract val type: DialogBodyType
 
-    override fun getNbt(): NBT {
+    override fun getNbt(): NBTCompound {
         return NBT.Compound { builder ->
             builder.put("type", type.getEntryIdentifier())
         }
