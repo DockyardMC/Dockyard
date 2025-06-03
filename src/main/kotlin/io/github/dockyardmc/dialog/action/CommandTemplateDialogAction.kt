340d3c12930eb0e6de2af7c40552b02c636b62a1
p1k0chu <anatoliyid0207@gmail.com>
2025-06-03 21:09:53 +0300

    dialogs DSL

--- src/main/kotlin/io/github/dockyardmc/dialog/action/CommandTemplateDialogAction.kt
+++ src/main/kotlin/io/github/dockyardmc/dialog/action/CommandTemplateDialogAction.kt
@@ -5,6 +5,15 @@ import io.github.dockyardmc.registry.registries.DialogActionType
 import io.github.dockyardmc.scroll.extensions.put
 import org.jglrxavpok.hephaistos.nbt.NBTCompound
 
+/**
+ * Command template will be proccessed by client and then executed as a command
+ *
+ * @property template The template.
+ *
+ * Example: `say $(arg1)`.
+ *
+ * will replace the `$(arg1)` with the contents of the input with key `arg1`
+ */
 class CommandTemplateDialogAction(val template: String) : DialogAction() {
     override val type: DialogActionType = DialogActionTypes.DYNAMIC_RUN_COMMAND
 
