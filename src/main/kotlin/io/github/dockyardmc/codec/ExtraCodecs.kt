1985814264cf19070d60aceeaa6be8601bd65aca
LukynkaCZE <jiraklukas058@seznam.cz>
2025-09-16 22:11:36 +0200

    update tide

--- src/main/kotlin/io/github/dockyardmc/codec/ExtraCodecs.kt
+++ src/main/kotlin/io/github/dockyardmc/codec/ExtraCodecs.kt
@@ -1,36 +1,13 @@
 package io.github.dockyardmc.codec
 
-import io.github.dockyardmc.extentions.readVarInt
-import io.github.dockyardmc.extentions.writeVarInt
 import io.github.dockyardmc.tide.codec.Codec
 import io.github.dockyardmc.tide.stream.StreamCodec
-import io.netty.buffer.ByteBuf
 
 object ExtraCodecs {
 
-    object LongArray {
-        val STREAM = object : StreamCodec<kotlin.LongArray> {
-
-            override fun write(buffer: ByteBuf, value: kotlin.LongArray) {
-                buffer.writeVarInt(value.size)
-                value.forEach { buffer.writeLong(it) }
-            }
-
-            override fun read(buffer: ByteBuf): kotlin.LongArray {
-                val size = buffer.readVarInt()
-                val longs = mutableListOf<Long>()
-                for (i in 0 until size) {
-                    longs.add(buffer.readLong())
-                }
-                return longs.toLongArray()
-            }
-        }
-
-        val CODEC = Codec.LONG_ARRAY
-    }
-
     object BitSet {
-        val STREAM = LongArray.STREAM.transform<java.util.BitSet>(java.util.BitSet::toLongArray, java.util.BitSet::valueOf)
-        val CODEC = LongArray.CODEC.transform<java.util.BitSet>(java.util.BitSet::valueOf, java.util.BitSet::toLongArray)
+        val STREAM = StreamCodec.LONG_ARRAY.transform<java.util.BitSet>(java.util.BitSet::toLongArray, java.util.BitSet::valueOf)
+        val CODEC = Codec.LONG_ARRAY.transform<java.util.BitSet>(java.util.BitSet::valueOf, java.util.BitSet::toLongArray)
     }
+
 }
--- src/main/kotlin/io/github/dockyardmc/protocol/packets/play/clientbound/ClientboundChunkDataPacket.kt
+++ src/main/kotlin/io/github/dockyardmc/protocol/packets/play/clientbound/ClientboundChunkDataPacket.kt
@@ -1,6 +1,5 @@
 package io.github.dockyardmc.protocol.packets.play.clientbound
 
-import io.github.dockyardmc.codec.ExtraCodecs
 import io.github.dockyardmc.protocol.packets.ClientboundPacket
 import io.github.dockyardmc.tide.stream.StreamCodec
 import io.github.dockyardmc.world.LightData
@@ -14,7 +13,7 @@ data class ClientboundChunkDataPacket(val x: Int, val z: Int, val heightmaps: Ma
         val STREAM_CODEC = StreamCodec.of(
             StreamCodec.INT, ClientboundChunkDataPacket::x,
             StreamCodec.INT, ClientboundChunkDataPacket::z,
-            StreamCodec.enum<ChunkHeightmap.Type>().mapTo(ExtraCodecs.LongArray.STREAM), ClientboundChunkDataPacket::heightmaps,
+            StreamCodec.enum<ChunkHeightmap.Type>().mapTo(StreamCodec.LONG_ARRAY), ClientboundChunkDataPacket::heightmaps,
             ChunkSection.BYTE_ARRAY_STREAM_CODEC, ClientboundChunkDataPacket::sections,
             BlockEntity.STREAM_CODEC.list(), ClientboundChunkDataPacket::blockEntities,
             LightData.STREAM_CODEC, ClientboundChunkDataPacket::light,
@@ -25,34 +24,4 @@ data class ClientboundChunkDataPacket(val x: Int, val z: Int, val heightmaps: Ma
     init {
         STREAM_CODEC.write(buffer, this)
     }
-
-//    init {
-//        //X Z
-//        buffer.writeInt(x)
-//        buffer.writeInt(z)
-//
-//        //Heightmaps
-//        buffer.writeMap<ChunkHeightmap.Type, List<Long>>(heightmaps.mapValues { map -> map.value.toList() }, ByteBuf::writeEnum, ByteBuf::writeLongArray)
-//
-//        //Chunk Sections
-//        val chunkSectionData = Unpooled.buffer()
-//        sections.forEach { section ->
-//            section.write(chunkSectionData)
-//        }
-//        buffer.writeByteArray(chunkSectionData.toByteArraySafe())
-//
-//        //Block Entities
-//        buffer.writeVarInt(blockEntities.size)
-//        blockEntities.forEach { blockEntity ->
-//            val id = blockEntity.blockEntityTypeId
-//            val point = ChunkUtils.chunkBlockIndexGetGlobal(blockEntity.positionIndex, 0, 0)
-//
-//            buffer.writeByte(((point.x and 15) shl 4 or (point.z and 15)))
-//            buffer.writeShort(point.y)
-//            buffer.writeVarInt(id)
-//            buffer.writeNBT(blockEntity.data)
-//        }
-//
-//        light.write(buffer)
-//    }
 }
