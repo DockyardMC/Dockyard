1eb964a99d6cd7825089571eb03d13e3649b4040
LukynkaCZE <jiraklukas058@seznam.cz>
2025-10-05 18:56:10 +0200

    more metadata rewrite

--- src/main/kotlin/io/github/dockyardmc/entity/metadata/MetadataType.kt
+++ src/main/kotlin/io/github/dockyardmc/entity/metadata/MetadataType.kt
@@ -1,7 +1,17 @@
 package io.github.dockyardmc.entity.metadata
 
 import io.github.dockyardmc.codec.ComponentCodecs
+import io.github.dockyardmc.codec.LocationCodecs
+import io.github.dockyardmc.codec.RegistryCodec
+import io.github.dockyardmc.item.ItemStack
+import io.github.dockyardmc.maths.Quaternion
+import io.github.dockyardmc.maths.vectors.Vector3f
+import io.github.dockyardmc.player.Direction
+import io.github.dockyardmc.player.EntityPose
+import io.github.dockyardmc.registry.registries.PaintingVariantRegistry
+import io.github.dockyardmc.registry.registries.ParticleRegistry
 import io.github.dockyardmc.tide.stream.StreamCodec
+import io.github.dockyardmc.world.block.Block
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -12,8 +22,22 @@ object MetadataType {
 
     val BYTE = next(StreamCodec.BYTE)
     val VAR_INT = next(StreamCodec.VAR_INT)
+    val OPTIONAL_VAR_INT = next(StreamCodec.VAR_INT.optional())
+    val BOOLEAN = next(StreamCodec.BOOLEAN)
+    val POSE = next(StreamCodec.enum<EntityPose>())
+    val VECTOR_3F = next(Vector3f.STREAM_CODEC)
+    val QUATERNION = next(Quaternion.STREAM_CODEC)
+    val BLOCK_STATE = next(Block.STREAM_CODEC)
+    val OPTIONAL_BLOCK_STATE = next(Block.STREAM_CODEC.optional())
+    val ITEM_STACK = next(ItemStack.STREAM_CODEC)
+    val BLOCK_POSITION = next(LocationCodecs.BLOCK_POSITION)
+    val OPTIONAL_BLOCK_POSITION = next(LocationCodecs.BLOCK_POSITION.optional())
+    val PARTICLE = next(RegistryCodec.stream(ParticleRegistry))
+    val DIRECTION = next(StreamCodec.enum<Direction>())
+    val PAINTING_VARIANT = next(RegistryCodec.stream(PaintingVariantRegistry))
 
     val OPTIONAL_COMPONENT = next(ComponentCodecs.STREAM.optional())
+    val COMPONENT = next(ComponentCodecs.STREAM)
     val FLOAT = next(StreamCodec.FLOAT)
 
     data class MetadataSerializer<T>(val type: Int, val streamCodec: StreamCodec<T>)
